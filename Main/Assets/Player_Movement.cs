4f76435afe8ecdfb3b9d70cb7d4ba3c706098e63
NicoHodgson <72668911+NicoHodgson@users.noreply.github.com>
2020-10-16 14:32:09 -0400

    Create Player_Movement.cs

---
+++ Main/Assets/Player_Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    Rigidbody rb;
+    private float moveSpeed = 3;
+
+    public const string RIGHT = "right";
+    public const string LEFT = "left";
+
+    string buttonPressed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Put non physics based movement in here
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            buttonPressed = RIGHT;
+
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            buttonPressed = LEFT;
+
+
+        }
+        else
+        {
+            buttonPressed = null;
+        }
+
+    }
+
+    //Put physica based movement in here
+    private void FixedUpdate()
+    {
+        if (buttonPressed == RIGHT)
+        {
+
+        }
+        else if (buttonPressed == LEFT)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
