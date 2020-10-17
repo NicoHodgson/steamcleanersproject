761663fb8a9597b8b1487efd5b6b9e34d422eae1
NicoHodgson <72668911+NicoHodgson@users.noreply.github.com>
2020-10-17 16:34:11 -0400

    Update Player_Movement.cs

--- Main/Assets/Player_Movement.cs
+++ Main/Assets/Player_Movement.cs
@@ -8,6 +8,9 @@ public class Player_Movement : MonoBehaviour
     public const string RIGHT = "right";
     public float moveSpeed = 3;
     string buttonpressed;
+    public bool isGrounded;
+    public float jumpForce;
+    public bool isJumping = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +18,11 @@ public class Player_Movement : MonoBehaviour
     }
     //Put physica based movement in here
     private void Update() {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             buttonpressed = LEFT;
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             buttonpressed = RIGHT;
         }
@@ -27,16 +30,33 @@ public class Player_Movement : MonoBehaviour
         {
             buttonpressed = null;
         }
-
-    }
+        if (Input.GetButtonDown("Jump") || Input.GetKey(KeyCode.W)) {
+            isJumping = true;
+        }
+        }
     private void FixedUpdate()
     {
         if (buttonpressed == RIGHT)
         {
-            rb.velocity =new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            rb.AddForce(new Vector3(moveSpeed * Time.deltaTime, 0, 0), ForceMode.Impulse);
         }
         else if (buttonpressed == LEFT) {
-            rb.velocity = new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+            rb.AddForce(new Vector3(-moveSpeed * Time.deltaTime, 0, 0), ForceMode.Impulse);
+        }
+        if (isJumping == true && isGrounded == true) {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
 }
