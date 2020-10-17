fe32c501e3c6d654109c1fb41bbbee3ca4fa6723
NicoHodgson <72668911+NicoHodgson@users.noreply.github.com>
2020-10-17 18:49:40 -0400

    Create collision.cs

---
+++ Main/Assets/Scripts/collision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+      if(other.gameObject.name == "Enemy") {
+	transform.position = new Vector3(-6.39f, -0.42f, 0f);
+      }
+    }
+}
