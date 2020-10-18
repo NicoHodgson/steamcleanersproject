e6234cca3dded562e61de077d7e5549aea565242
NicoHodgson <72668911+NicoHodgson@users.noreply.github.com>
2020-10-18 13:38:49 -0400

    Added Camera Functionatlity

---
+++ Main/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    float speedOffset;
+    [SerializeField]
+    Vector3 posOffset;
+
+    public float leftLimit;
+    public float rightLimit;
+    public float bottomLimit;
+    public float topLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera start pos
+        Vector3 startPos = transform.position;
+        //player start pos
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -5;
+
+        transform.position = Vector3.Lerp(startPos, endPos, speedOffset * Time.deltaTime);
+
+        transform.position = new Vector3
+            (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+            );
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //top line
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        //right line
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        //bottom line
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        //left line
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+    }
+}
