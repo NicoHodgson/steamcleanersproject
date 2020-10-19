09c785d9d4046321bdf4157f7fbe303bb9ce687f
NicoHodgson <72668911+NicoHodgson@users.noreply.github.com>
2020-10-18 21:42:07 -0400

    Fixed the move script to have vertical options and adjustable ranges

--- Main/Assets/Scripts/move.cs
+++ Main/Assets/Scripts/move.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public float speed = 1.19f;
+    public float displacement = 3;
+    public bool Vertical = false;
+    public bool Horizantal = false;
     Vector3 pointA;
     Vector3 pointB;
 
     // Start is called before the first frame update
     void Start()
     {
-      pointA = new Vector3(0, -2.83f, 0);
-      pointB = new Vector3(5, -2.83f, 0);
+        if (Horizantal)
+        {
+            pointA = new Vector3(transform.position.x + displacement, transform.position.y, transform.position.z);
+            pointB = new Vector3(transform.position.x - displacement, transform.position.y, transform.position.z);
+        }
+        else if (Vertical)
+        {
+            pointA = new Vector3(transform.position.x, transform.position.y + displacement, transform.position.z);
+            pointB = new Vector3(transform.position.x, transform.position.y - displacement, transform.position.z);
+        }
     }
 
     // Update is called once per frame
